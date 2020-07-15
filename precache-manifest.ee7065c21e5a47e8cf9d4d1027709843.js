4e80118c8d662b6ec0ac95e7f4b0eec94bdeea8a
yuchaehwi <nancy1022@naver.com>
2020-07-15 21:56:10 +0900

    Updates

---
+++ precache-manifest.ee7065c21e5a47e8cf9d4d1027709843.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bdfaf5ddc0c4df68091bac551e6d5a2",
+    "url": "/spa-github-pages/index.html"
+  },
+  {
+    "revision": "c2f666093b9618a3d0b5",
+    "url": "/spa-github-pages/static/css/main.285a1adb.chunk.css"
+  },
+  {
+    "revision": "63c350051614baff2f60",
+    "url": "/spa-github-pages/static/js/2.ba22a582.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spa-github-pages/static/js/2.ba22a582.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2f666093b9618a3d0b5",
+    "url": "/spa-github-pages/static/js/main.70a6bdaa.chunk.js"
+  },
+  {
+    "revision": "abb57be12bb85b7e5573",
+    "url": "/spa-github-pages/static/js/runtime-main.03533089.js"
+  },
+  {
+    "revision": "26453788a4d8d1ed88c2524b96d93b66",
+    "url": "/spa-github-pages/static/media/bgImage.26453788.png"
+  },
+  {
+    "revision": "0a06af8bdf0dde1485114249d682a453",
+    "url": "/spa-github-pages/static/media/portfolioImg.0a06af8b.png"
+  }
+]);
